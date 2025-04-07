3acb727911104a7be7f91c7de39ae618dc58a6d7
Douglas Rengel Pazinatto <115743267+drPazinatto@users.noreply.github.com>
2025-04-07 21:07:02 +0000

    Atualização Desafio

---
+++ TEMA 3/DesafioNovato.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    // Movimento da Torre (5 casas para a direita)
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+    printf("\n");
+
+    // Movimento do Bispo (5 casas na diagonal para cima e à direita)
+    printf("Movimento do Bispo:\n");
+    int j = 0;
+    while (j < 5) {
+        printf("Cima, Direita\n");
+        j++;
+    }
+    printf("\n");
+
+    // Movimento da Rainha (8 casas para a esquerda)
+    printf("Movimento da Rainha:\n");
+    int k = 0;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k < 8);
+    printf("\n");
+
+    return 0;
+}
